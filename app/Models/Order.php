29f6f71dc60a84c3003949395a0e1aec7cfa9157
Azazel Araiza <115805096+Kravitz-araiza@users.noreply.github.com>
2023-11-10 04:05:57 -0600

    Update Order.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -33,9 +34,15 @@ class Order extends Model
     ];
 
 
+
     public $with = [
         'user'
     ];
+
+    public function getCreatedAtAttribute($date)
+    {
+        return Carbon::createFromFormat('Y-m-d H:i:s', $date)->format('Y-m-d H:i');
+    }
 
     public function user()
     {
