85c5b21b026040797176e87e405f7ac1bdd2e777
Azazel Araiza <115805096+Kravitz-araiza@users.noreply.github.com>
2023-11-16 06:22:58 -0600

    Add quantity

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -15,13 +15,13 @@ class Product extends Model
         'subtitle',
         'description',
         'price',
+        'quantity',
         'company_id',
         'image',
         'isActive',
     ];
     protected $appends = [
         'principal_image',
-        'gallery'
     ];
     public function scopeActive($q)
     {
@@ -32,10 +32,6 @@ class Product extends Model
         $q->where('company_id', $id);
     }
     public function getPrincipalImageAttribute()
-    {
-        return $this->image;
-    }
-    public function getGalleryAttribute()
     {
         return $this->image;
     }
